888dae863c38db60bd65b4d905626e91ed321765
Blake Hagen <blakehagen@gmail.com>
2015-10-31 16:00:36 -0600

    app1 added input box

--- app1 - basics/controller.js
+++ app1 - basics/controller.js
@@ -2,8 +2,15 @@ angular.module('app1').controller('mainCtrl', function ($scope, mainService) {
 
 
     $scope.bananaInfo = mainService.returnData();
-    
-    
+
+    $scope.submit = function () {
+        mainService.addData($scope.someText);
+        $scope.someText = '';
+    }
+
+
+
+
 
 
 
--- app1 - basics/service.js
+++ app1 - basics/service.js
@@ -1,10 +1,10 @@
 angular.module('app1').service('mainService', function () {
 
     var arr = [
-        // 'banana 1',
-        // 'banana 2',
-        // 'banana 3',
-        // 'chimpanzee'
+        'banana 1',
+        'banana 2',
+        'banana 3',
+        'chimpanzee'
     ];
 
 
@@ -12,10 +12,15 @@ angular.module('app1').service('mainService', function () {
 
 
     this.returnData = function () {
-        for (var i = 1; i <= 100; i++) {
-            arr.push("banana " + i);
-        }
+        // for (var i = 1; i <= 100; i++) {
+        //     arr.push("banana " + i);
+        // }
         return arr;
+    }
+    
+    
+    this.addData = function(data){
+        arr.push(data);
     }
 
 
